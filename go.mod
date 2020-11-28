571a162eb7bdf1d16bc5d8d687ad3dc44d3442b7
K.Tsukimoto <t.29.wuq@gmail.com>
2020-11-28 19:32:53 +0900

    :heavy_plus_sign: add router

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kindai-csg/d-blog-engine
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/kindai-csg/d-blog-engine/infrastructure"
+
+func main() {
+    infrastructure.Router.Run(":2640")
+}
